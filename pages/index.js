cd12a7b92e5ef2f4fa6de435b64052a0f504ad06
carolynk113 <companiondogregistry@gmail.com>
2025-09-03 11:11:40 -0400

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 24, fontFamily: "sans-serif" }}>
+      <h1>Puppy Weight Tracker</h1>
+      <p>It works 🎉</p>
+    </main>
+  );
+}
